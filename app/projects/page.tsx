83ec166bab3a0f7d32df9e375a06c12ee35c1955
vikashkhati007 <vikashkhati007@gmail.com>
2023-10-17 13:02:32 +0530

    done

--- app/projects/page.tsx
+++ app/projects/page.tsx
@@ -1,11 +1,15 @@
 import CardBox from "@/components/project/Card";
 import React from "react";
 import { ScrollArea } from "@/components/ui/scroll-area";
+import { Separator } from "@/components/ui/separator";
 const page = () => {
   return (
     <>
       <div className="w-full h-screen flex justify-around items-center gap-5">
         <ScrollArea className="w-[90%] md:w-[80%] m-auto h-[70%] rounded-md">
+          <div className="titlecontainer flex flex-col items-center justify-center ">
+          <p className="font-bold text-2xl">Projects</p>
+          </div>
           <div className="cardboxcontainer flex flex-wrap justify-around items-center my-5 gap-5">
             <CardBox />
             <CardBox />
