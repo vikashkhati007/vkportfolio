e2dd6d9f53b3575e9c8791e47fe70621f3dd5b1e
Vikash Khati <124573409+vikashkhati007@users.noreply.github.com>
2024-05-08 20:32:51 +0000

    sparkle added

--- components/body/Intro.tsx
+++ components/body/Intro.tsx
@@ -1,4 +1,5 @@
 import Link from "next/link";
+import { SparklesPreview } from "../effects/sparkle";
 
 const Intro = () => {
   return (
@@ -10,12 +11,8 @@ const Intro = () => {
         id="texture"
         className="backgroundtexture w-full h-screen absolute"
       ></div>
-      <div className="w-12 h-12 bg-gradient-to-tr from-blue-400 to-blue-700 absolute left-40 hidden md:block blur-2xl backdrop-filter rounded-full"></div>
-      <div className="w-12 h-12 bg-gradient-to-tr from-pink-400 to-pink-700 absolute right-40 hidden md:block blur-2xl backdrop-filter rounded-full "></div>
-      <div className="w-12 h-12 bg-gradient-to-tr from-fuchsia-400 to-fuchsia-700 absolute  top-24 blur-2xl backdrop-filter rounded-full "></div>
-      <div className="w-12 h-12 bg-gradient-to-tr from-green-400 to-green-700 absolute  bottom-24 blur-2xl backdrop-filter rounded-full "></div>
-
-      <div className="flex flex-col items-center justify-center">
+      <SparklesPreview />
+      <div className="flex flex-col items-center justify-center relative dark:bg-black drk:bg-blend-screen border-white">
         <div className="textcontainer flex justify-center items-center">
           <h1 className="text-3xl sm:text-4xl md:text-5xl text-center font-bold text-primary">
             <span className="bg-gradient-to-l from-blue-700 to-blue-400 text-transparent bg-clip-text">
@@ -24,7 +21,7 @@ const Intro = () => {
             to my Portfolio
           </h1>
         </div>
-        <p className="mt-4 text-md sm:text-lg md:text-xl text-gray-400">
+        <p className="text-md sm:text-lg md:text-xl text-gray-400 mt-2">
           I am a Full Stack Website Developer
         </p>
       </div>
--- components/body/projects/Box.tsx
+++ components/body/projects/Box.tsx
@@ -1,40 +1,48 @@
 import Image from "next/image";
 import Link from "next/link";
 import React from "react";
+import { HoverBorderGradient } from "../../ui/moving-border";
 
 const Box = ({ image, htitle, title, description, link }: any) => {
   return (
     <>
-      <div
-        id="project"
-        className="projectcontainer h-fit w-[90%] m-auto md:m-0 xs:w-[300px] sm:w-[300px] md:h-[300px] overflow-hidden bg-background rounded-md border-2 border-blue-400 shadow-sm hover:scale-105 transition"
+      <HoverBorderGradient
+        containerClassName="rounded-sm"
+        as="button"
+        className="dark:bg-black bg-white text-black dark:text-white"
       >
-        <Image
-          width={300}
-          height={300}
-          src={`${image}`}
-          alt="My Image"
-          className="border-b w-full"
-        />
-        <div className="aboutcontainer space-y-2 p-3">
-          <div className="title flex justify-between">
-            <h1 className="text-xl font-semibold text-primary">
-              <span className="bg-gradient-to-l from-blue-600 to-blue-500 bg-clip-text text-transparent">
-                {htitle}
-              </span>{" "}
-              {title}
-            </h1>
-            <Link href={link}>
-              <button className="rounded-s-full border bg-gradient-to-r from-gray-700 to-slate-500 px-2 font-mono font-normal text-white cursor-pointer hover:from-blue-300 hover:to-blue-500 ">
-                LINK
-              </button>
-            </Link>
+        <div
+          id="project"
+          className="projectcontainer h-fit w-[90%] m-auto md:m-0 xs:w-[300px] sm:w-[300px] md:h-[300px] overflow-hidden bg-background rounded-md shadow-sm hover:scale-105 transition"
+        >
+
+          <Image
+            width={300}
+            height={300}
+            src={`${image}`}
+            alt="My Image"
+            className="border-b w-full"
+          />
+          <div className="aboutcontainer space-y-2 p-3">
+            <div className="title flex justify-between">
+              <h1 className="text-xl font-semibold text-primary">
+                <span className="bg-gradient-to-l from-blue-600 to-blue-500 bg-clip-text text-transparent">
+                  {htitle}
+                </span>{" "}
+                {title}
+              </h1>
+              <Link href={link}>
+                <button className="rounded-s-full border bg-gradient-to-r from-gray-700 to-slate-500 px-2 font-mono font-normal text-white cursor-pointer hover:from-blue-300 hover:to-blue-500 ">
+                  LINK
+                </button>
+              </Link>
+            </div>
+            <p className="text-sm font-normal text-primary">
+              {description.substring(0, 150) + "..."}
+            </p>
           </div>
-          <p className="text-sm font-normal text-primary">
-            {description.substring(0, 150) + "..."}
-          </p>
         </div>
-      </div>
+      </HoverBorderGradient>
     </>
   );
 };
