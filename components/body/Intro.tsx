9f4767a5bb8a65d394a600b2215fa32430ff4a87
Vikash Khati <124573409+vikashkhati007@users.noreply.github.com>
2024-05-10 12:05:14 +0000

    fonts upgraded and next updated

--- components/body/Intro.tsx
+++ components/body/Intro.tsx
@@ -1,6 +1,10 @@
 "use client";
 import { motion } from "framer-motion";
 import { HeroHighlight, Highlight } from "../ui/hero-highlight";
+import { Rowdies } from "next/font/google";
+import { cn } from "@/lib/utils";
+const rowdies = Rowdies({ subsets: ["latin"], weight: ["300"] });
+
 const Intro = () => {
   return (
     <section
@@ -8,30 +12,31 @@ const Intro = () => {
       className="flex flex-col items-center justify-center w-full h-screen"
     >
       <div className="flex flex-col items-center justify-center relative border-white">
-      <HeroHighlight>
-      <motion.h1
-        initial={{
-          opacity: 0,
-          y: 20,
-        }}
-        animate={{
-          opacity: 1,
-          y: [20, -5, 0],
-        }}
-        transition={{
-          duration: 0.5,
-          ease: [0.4, 0.0, 0.2, 1],
-        }}
-        className="md:text-4xl lg:text-5xl font-bold text-neutral-700 dark:text-white max-w-4xl text-center mx-auto "
-      >
-       <h1 className="text-5xl sm:text-6xl md:text-7xl font-bold text-primary mb-3 lg:mb-0 ">
-            Vikash Khati
-          </h1>
-        <Highlight className="pacifico-regular text-black text-xl sm:text-2xl md:text-3xl dark:text-white px-2 backdrop-blur-sm flex justify-center items-center mx-10">
-          full stack web developer
-        </Highlight>
-      </motion.h1>
-    </HeroHighlight>
+        <h1 className={cn("text-5xl sm:text-6xl md:text-7xl font-bold text-primary mb-4 lg:mb-1", rowdies.className)} >
+          Vikash Khati
+        </h1>
+        <HeroHighlight>
+          <motion.h1
+            initial={{
+              opacity: 0,
+              y: 20,
+            }}
+            animate={{
+              opacity: 1,
+              y: [20, -5, 0],
+            }}
+            transition={{
+              duration: 0.5,
+              ease: [0.4, 0.0, 0.2, 1],
+            }}
+            className="md:text-4xl lg:text-5xl font-bold text-neutral-700 dark:text-white max-w-4xl text-center mx-auto "
+          >
+
+            <Highlight className="pacifico-regular text-black text-xl sm:text-2xl md:text-3xl dark:text-white px-2 backdrop-blur-sm flex justify-center items-center mx-10">
+              full stack web developer
+            </Highlight>
+          </motion.h1>
+        </HeroHighlight>
       </div>
     </section>
   );
